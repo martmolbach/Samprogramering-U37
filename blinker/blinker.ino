1fde6db66607b0809a97b5ae97750149975b68c6
martmolbach <martmolb@stud.ntnu.no>
2023-09-14 15:23:39 +0200

    Adds led

--- blinker/blinker.ino
+++ blinker/blinker.ino
@@ -1,8 +1,18 @@
-void setup(){
-    Serial.begin(9600);
+const int LED_PIN = 13;
+
+void blinkLed() {
+    digitalWrite(LED_PIN, HIGH);
+    delay(1000);
+    digitalWrite(LED_PIN, LOW);
+    delay(1000);
+
 }
 
-void loop(){
-    Serial.println("B");
-    delay(1000);
+void setup() {
+    Serial.begin(9600);
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+    blinkLed();
 }
